d7cd79a6a7b38d1554863bfa909b3032923deae8
Dimitri (Dimi) Tarasowski <33748291+tarasowski@users.noreply.github.com>
2024-05-13 12:59:56 +0200

    Update if-else-string.sh

--- module-1/if-else-string.sh
+++ module-1/if-else-string.sh
@@ -5,7 +5,8 @@ string1="true"
 string2="true"
 
 # Compare the strings
-if [[ "$string1" == "$string2" ]]; then
+if [[ "$string1" == "$string2" ]] 
+then
     echo "Strings are equal"
 else
     echo "Strings are not equal"
