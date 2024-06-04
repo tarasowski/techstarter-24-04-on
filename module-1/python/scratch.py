f3cb367418dc910307827b04b460a7483ef2f3e4
Dimitri (Dimi) Tarasowski <33748291+tarasowski@users.noreply.github.com>
2024-06-04 09:24:31 +0200

    Update scratch.py

--- module-1/python/scratch.py
+++ module-1/python/scratch.py
@@ -1,1 +1,7 @@
+names = ["Marcus", "Kaho", "Ilona"] # array / list
+satz = "Hallo hier sind die Namen: Marcus, Kaho, Ilona"
+length = len(names)
+length_satz = len(satz)
 
+print(length)
+print(length_satz)
