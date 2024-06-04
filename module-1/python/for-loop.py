853eabb966925975a0e57bde051a2f8955a21ffb
tarasowski <dimitri+github@tarasowski.de>
2024-06-04 09:36:38 +0200

    feat: added for loop

---
+++ module-1/python/for-loop.py
@@ -0,0 +1,11 @@
+import time
+
+brands = ["Mercedes", "Porsche", "BMW", "VW", "Opel"]
+
+
+# for das keywords reservier von python
+# mark ist die variable die temporär ist, nimmt immer einen neuen wert beim durchlauf
+# brands ist halt hier die liste s. oben 
+for marke in brands:
+  print(marke)
+  time.sleep(3)
