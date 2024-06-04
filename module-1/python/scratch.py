14bb8dba9220dd20f0293d25e62e5e289897b53a
Dimitri (Dimi) Tarasowski <33748291+tarasowski@users.noreply.github.com>
2024-06-04 09:49:03 +0200

    Update scratch.py

--- module-1/python/scratch.py
+++ module-1/python/scratch.py
@@ -1,7 +1,27 @@
+import time
+
+brands = ["Mercedes", "Porsche", "BMW", "VW", "Opel"]
+empty_list = []
+empty_list2 = list("10", "10")
+print(empty_list)
+print(empty_list2)
+# for das keywords reservier von python
+# mark ist die variable die temporär ist, nimmt immer einen neuen wert beim durchlauf
+# brands ist halt hier die liste s. oben 
+for marke in brands:
+  print(marke + "!!!")
+  print("2")
+  
+"""
+for name in ["Marcus", "Kaho", "Ilona"]:
+  print("hier ist der name: ", name)
+"""  
+  
 names = ["Marcus", "Kaho", "Ilona"] # array / list
 satz = "Hallo hier sind die Namen: Marcus, Kaho, Ilona"
 length = len(names)
 length_satz = len(satz)
 
-print(length)
-print(length_satz)
+#print(length)
+#print(length_satz)
+  
