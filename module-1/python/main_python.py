957313ce75beaca530e07beb9aa2d1d6032be640
tarasowski <dimitri+github@tarasowski.de>
2024-06-10 12:51:17 +0200

    feat: added open() function

---
+++ module-1/python/main_python.py
@@ -0,0 +1,14 @@
+import json
+import os
+import sys
+
+import math # standard module von python
+import random 
+
+num = random.randint(1, 100) 
+print(f"Random integer between 1 and 100: {num}") 
+
+fruits = ["Java", "C", "C++", "Python"] 
+chosen_fruit = random.choice(fruits) 
+print(f"Randomly chosen language: {chosen_fruit}") 
+
