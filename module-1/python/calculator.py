42bfb08293b9a4ba7e6e093cd1f07bbcac50c132
Dimitri (Dimi) Tarasowski <33748291+tarasowski@users.noreply.github.com>
2024-06-26 10:13:38 +0200

    Create calculator.py

---
+++ module-1/python/calculator.py
@@ -0,0 +1,39 @@
+# taschenrechner --math-- modulen erstellen
+# add - ohne math lösen
+# sub - ohne math lösen
+# mul - ohne math lösen
+# div  - ohen math lösen
+# Quadratwurzel
+# Potenzierung
+import math
+
+num1 = float(input("Bitte gebe die ersten Zahl an: "))
+num2 = float(input("Bitte gebe die zweiten Zahl an: "))
+
+operation = input(
+    "Bitte gebe die gewünschen Mathoperation an mit +, -, *, /, sqrt, **: ")
+
+erlaubt = ["+", "-", "*", "/", "sqrt", "**"]
+ergebnis = 0
+
+if operation not in erlaubt:
+    print("Das ist keine erlaubte Operation. Bitte gebe eine der folgenden Operationen an: +, -, *, /, sqrt, **: ")
+    exit()
+
+if operation == "+":
+    ergebnis = (num1 + num2)  # wie kann ich das hier mit math machen???
+elif operation == "-":
+    ergebnis = (num1 - num2)
+elif operation == "/":
+    if num2 == 0:
+        print("Das geht nicht, weil du durch 0 teilen willst.")
+    else:
+        ergebnis = (num1 / num2)
+elif operation == "*":
+    ergebnis = (num1 * num2)
+elif operation == "sqrt":
+    ergebnis = math.sqrt(num1)
+elif operation == "**":
+    ergebnis = (num1 ** num2)  # ergebnis = math.pow(num1, num2)
+
+print(num1, num2, operation, ergebnis)
