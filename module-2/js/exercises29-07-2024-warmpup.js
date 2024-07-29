a85e3fdc378d62402f39d77484b4556bb96366fc
Dimitri (Dimi) Tarasowski <33748291+tarasowski@users.noreply.github.com>
2024-07-29 08:16:34 +0200

    Create exercises29-07-2024-warmpup.js

---
+++ module-2/js/exercises29-07-2024-warmpup.js
@@ -0,0 +1,7 @@
+/*
+Aufgabe 1: Element hinzufügen
+Beschreibung: Schreibe eine Funktion addElementToArray, die ein Element zu einem bestehenden Array hinzufügt.
+Beispiel:
+Input: ([1, 2, 3], 4)
+Output: [1, 2, 3, 4]
+*/
