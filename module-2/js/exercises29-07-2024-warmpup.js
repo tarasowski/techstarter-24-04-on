bca526f905a9dd7bd481f6ab9adca7994f18e940
Dimitri (Dimi) Tarasowski <33748291+tarasowski@users.noreply.github.com>
2024-07-29 08:19:46 +0200

    Update exercises29-07-2024-warmpup.js

--- module-2/js/exercises29-07-2024-warmpup.js
+++ module-2/js/exercises29-07-2024-warmpup.js
@@ -5,3 +5,43 @@ Beispiel:
 Input: ([1, 2, 3], 4)
 Output: [1, 2, 3, 4]
 */
+
+/*
+Aufgabe 2: Summe der Elemente
+Beschreibung: Schreibe eine Funktion sumArray, die die Summe aller numerischen Elemente in einem Array berechnet.
+Beispiel:
+Input: [1, 2, 3, 4]
+Output: 10
+*/
+
+/*
+Aufgabe 4: Kleinste und größte Zahl finden
+Beschreibung: Schreibe eine Funktion findMinMax, die die kleinste und die größte Zahl in einem Array zurückgibt.
+Beispiel:
+Input: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+Output: { min: 1, max: 9 }
+*/
+
+/*
+Aufgabe 1: Eigenschaft lesen
+Beschreibung: Schreibe eine Funktion getProperty, die den Wert einer bestimmten Eigenschaft eines Objekts zurückgibt.
+Beispiel:
+Input: { name: 'Max', age: 28 }, 'name'
+Output: 'Max'
+*/
+
+/*
+Aufgabe 2: Eigenschaftswerte summieren
+Beschreibung: Schreibe eine Funktion sumObjectValues, die die Summe aller numerischen Werte in einem Objekt berechnet.
+Beispiel:
+Input: { a: 1, b: 2, c: 3 }
+Output: 6
+*/
+
+/*
+Aufgabe 4: Eigenschaften zählen
+Beschreibung: Schreibe eine Funktion countProperties, die die Anzahl der Eigenschaften in einem Objekt zählt.
+Beispiel:
+Input: { name: 'Max', age: 28, job: 'Developer' }
+Output: 3
+*/
